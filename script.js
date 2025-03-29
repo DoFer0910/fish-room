e23f634418f229fd9cfb62857e05527094c9616e
DoFer0910 <hiratti20050813@gmail.com>
2025-03-30 02:38:35 +0900

    ver 1.0
    基本的なサイトの内容を作成

---
+++ script.js
@@ -0,0 +1,14 @@
+// スクロールでフェードイン
+document.addEventListener('DOMContentLoaded', function() {
+  const fadeElements = document.querySelectorAll('.scroll-fade-in');
+  const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('visible');
+      }
+    });
+  }, {
+    threshold: 0.1
+  });
+  fadeElements.forEach(el => observer.observe(el));
+}); 
